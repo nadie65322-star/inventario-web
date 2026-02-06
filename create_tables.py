6b39a259157efa9ce76988dc27003389743ba431
David <nadie65322@gmail.com>
2026-02-05 22:52:32 -0500

    Ingorar create_tables

--- create_tables.py
+++
@@ -1,8 +0,0 @@
-from inventario import create_app, db
-from inventario.models import Item, Movimiento, GuardadoManual, GuardadoManualItem
-
-app = create_app()
-
-with app.app_context():
-    db.create_all()
-    print("Tablas creadas correctamente")
